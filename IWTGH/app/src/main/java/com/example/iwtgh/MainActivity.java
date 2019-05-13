93434a98f880f32bb8bb15758a87caebb8de4726
iye0n <nay0824@korea.ac.kr>
2019-05-13 21:52:27 +0900

    gogo

---
+++ IWTGH/app/src/main/java/com/example/iwtgh/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.iwtgh;
+
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.activity_main);
+    }
+}
+
