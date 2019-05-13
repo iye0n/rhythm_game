8fdc8c9df8812362911bf394af9cac33a6ebe4b3
iye0n <nay0824@korea.ac.kr>
2019-05-13 22:24:06 +0900

    leenayeon

--- IWTGH/app/src/main/java/com/example/iwtgh/MainActivity.java
+++ IWTGH/app/src/main/java/com/example/iwtgh/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.iwtgh;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
 }
