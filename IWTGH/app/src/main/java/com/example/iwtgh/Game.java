61bf1dcc80d512bc6a329fc98accc16a5b7f4573
iye0n <nay0824@korea.ac.kr>
2019-05-22 18:45:16 +0900

    update

--- IWTGH/app/src/main/java/com/example/iwtgh/Game.java
+++ IWTGH/app/src/main/java/com/example/iwtgh/Game.java
@@ -19,6 +19,9 @@ import android.widget.TextView;
 import static com.example.iwtgh.R.id.basicnode;
 
 class MyView extends View {
+
+    int x = 0, y = 0;
+
     public MyView(Context context) {
         super(context);
     }
@@ -35,12 +38,46 @@ class MyView extends View {
         BitmapDrawable basicn = null;
         basicn =(BitmapDrawable) getResources().getDrawable(R.drawable.nodebasic, null);
         Bitmap bit = basicn.getBitmap();
-        canvas.drawBitmap(bit,300,100,null);
+        canvas.drawBitmap(bit, x, y,null);
+    }
+}
+
+class GameObject{
+
+    GameObject(MyView mv, String Nodetype, String Songtype, Music a1) throws InterruptedException {
+
+        drop(mv, Nodetype);
+
+        Thread.sleep(10000);
+        a1.stopMusic();
+    }
+
+    public void drop(MyView mv, String Nodetype) throws InterruptedException {
+
+        switch(Nodetype){
+            case "1":
+                mv.x = 300; mv.y = 50;
+                break;
+            case "2":
+                mv.x = 630; mv.y = 50;
+                break;
+            case "3":
+                mv.x = 950; mv.y = 50;
+                break;
+            case "4":
+                mv.x = 1270; mv.y = 50;
+        }
+
+        for(int i=0; i<100; i++){
+            mv.y++;
+            Thread.sleep(100);
+            mv.invalidate();
+        }
     }
 }
 
 public class Game extends Activity {
-    MyView mv;
+
     protected void onCreate(Bundle savedInstanceState) {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         super.onCreate(savedInstanceState);
@@ -54,33 +91,55 @@ public class Game extends Activity {
         String m = intent.getStringExtra("title");
         // Intent i = new Intent(getApplicationContext(), Node.class);
 
+        MyView mv;
+        setContentView(R.layout.activity_game);
+        mv = (MyView) findViewById(basicnode);
         switch (m) {
             case "고고베베 - 마마무":
-                setContentView(R.layout.activity_game);
                 Music a1 = new Music(this, "gogobebe", false);
-                mv = (MyView) findViewById(basicnode);
                 mv.invalidate();
+                try {
+                    new GameObject(mv, "4", "1", a1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 //GameObject를 만들고 그 안에서 노래를 스탑시키고 나옴
                 break;
             case "답을 찾지 못한 날 - 윤하":
                 Music a2 = new Music(this, "nodab", false);
-                mv = (MyView) findViewById(basicnode);
                 mv.invalidate();
+                try {
+                    new GameObject(mv, "4", "2", a2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 break;
             case "STAY - BlackPink":
                 Music a3 = new Music(this, "stay", false);
-                mv = (MyView) findViewById(basicnode);
                 mv.invalidate();
+                try {
+                    new GameObject(mv, "4", "3", a3);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 break;
             case "Perfect - Ed Sheeran":
                 Music a4 = new Music(this, "perfect", false);
-                mv = (MyView) findViewById(basicnode);
                 mv.invalidate();
+                try {
+                    new GameObject(mv, "4", "4", a4);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 break;
             case "Bad guy - Billie Eilish":
                 Music a5 = new Music(this, "badguy", false);
-                mv = (MyView) findViewById(basicnode);
                 mv.invalidate();
+                try {
+                    new GameObject(mv, "4", "5", a5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 break;
         }
     }
